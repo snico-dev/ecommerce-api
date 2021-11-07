eb88c79a0a8098bb9e167df46863e428c788fe8d
Nicolas Silva dos Santos <nicolas.ssantos@stone.com.br>
2021-11-07 12:51:55 -0300

    feat: create filters

---
+++ GetApi.Ecommerce.UnitTests/Helpers/MoqHelpers.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using Moq;
+using System;
+using System.Linq.Expressions;
+
+
+namespace GetApi.Ecommerce.UnitTests.Helpers
+{
+    public static class MoqHelpers
+    {
+        public static ExpressionFilterDefinition<T> IsValidFilter<T>(Expression<Func<T, bool>> expression)
+        {
+            return It.Is<ExpressionFilterDefinition<T>>(expr => IsEquals(expr, new ExpressionFilterDefinition<T>(expression)));
+        }
+
+        private static bool IsEquals<T>(this ExpressionFilterDefinition<T> expr, ExpressionFilterDefinition<T> comparerExpr)
+        {
+            var serializerRegistry = BsonSerializer.SerializerRegistry;
+            var documentSerializer = serializerRegistry.GetSerializer<T>();
+
+            return expr.Render(documentSerializer, serializerRegistry) == comparerExpr.Render(documentSerializer, serializerRegistry);
+        }
+    }
+}
---
+++ GetApi.Ecommerce.UnitTests/Infra/Core/Catalog/Repositoreis/CategoryRepositoryTests.cs
@@ -0,0 +1,88 @@
+﻿using AutoFixture;
+using GetApi.Ecommerce.Core.Catalog.Entities;
+using GetApi.Ecommerce.UnitTests.Extensions;
+using GetApi.Ecommerce.Infra.Core.Catalog.Repositoreis;
+using MongoDB.Driver;
+using Moq;
+using System;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using System.Collections.Generic;
+using GetApi.Ecommerce.UnitTests.Helpers;
+
+namespace GetApi.Ecommerce.UnitTests.Infra.Core.Catalog.Repositoreis
+{
+    public class CategoryRepositoryTests
+    {
+        private Mock<IMongoCollection<Category>> _collectionMock = new Mock<IMongoCollection<Category>>();
+        private Fixture _fixture = new Fixture();
+
+        [Fact]
+        public async Task Given_Category_When_Create_Should_InsertCategory()
+        {
+            // arrange
+            var category = _fixture.Create<Category>();
+            var cancellationToken = new CancellationToken();
+
+            _collectionMock
+               .Setup(x => x.InsertOneAsync(
+                   It.Is<Category>(x => x.Id == category.Id),
+                   It.Is<InsertOneOptions>(x => x.BypassDocumentValidation == false),
+                   cancellationToken)
+               )
+               .Returns(Task.CompletedTask)
+               .Verifiable();
+
+            // act
+            await GetRepository().Create(category, cancellationToken);
+
+            // assert
+            _collectionMock.VerifyAll();
+        }
+
+
+        [Fact]
+        public async Task Given_Filter_When_Find_Should_ReturnsCategory()
+        {
+            // arrange
+            var category = _fixture.Create<Category>();
+            var cancellationToken = new CancellationToken();
+
+            var cursorMock = new Mock<IAsyncCursor<Category>>();
+            var findOptions = new FindOptions<Category> { };
+
+            cursorMock
+                .Setup(x => x.MoveNextAsync(cancellationToken))
+                .ReturnsAsync(true)
+                .Verifiable();
+
+            cursorMock
+                .SetupGet(x => x.Current)
+                .Returns(new List<Category> { category })
+                .Verifiable();
+
+            _collectionMock
+               .Setup(x => x.FindAsync(
+                   MoqHelpers.IsValidFilter<Category>(x => x.Id == category.Id),
+                   It.Is<FindOptions<Category>>(x => x.BatchSize == findOptions.BatchSize),
+                   cancellationToken)
+               )
+               .ReturnsAsync(cursorMock.Object)
+               .Verifiable();
+
+            // act
+            await GetRepository().Find(x => x.Id == category.Id, cancellationToken);
+
+            // assert
+            _collectionMock.VerifyAll();
+            cursorMock.VerifyAll();
+        }
+
+        public CategoryRepository GetRepository()
+        {
+            return new CategoryRepository(_collectionMock.Object);
+        }
+    }
+}
